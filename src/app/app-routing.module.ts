42c4cbd7e5598b76571c877d13814f627d875ec7
Olacoded <apple@Olacoded-MBPro-2.local>
2020-12-05 15:38:37 +0100

    UI complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './auth/auth.component'
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 // const routes: Routes = [];
  const routes: Routes = [
   {
-    path: '',  component: AuthComponent
+    path: '',  component: AuthComponent,
     
+  },
+  {
+    path: 'dashboard', component: DashboardComponent 
   }
 ]
 
